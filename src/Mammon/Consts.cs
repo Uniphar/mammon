6b11d6ab2460c8b26b3bed8fff3827551a548022
VaclavK <vaclav@vaclavkabat.net>
2024-05-21 11:55:41 +0100

    rename config environment variable name

--- src/Mammon/Consts.cs
+++ src/Mammon/Consts.cs
@@ -4,7 +4,7 @@ public static class Consts
 {
     public const string StateStoreName = "statestore";
 
-    public const string ConfigKeyVaultConfigEnvironmentVariable  = "CONFIG_KEYVAULT_URL";
+    public const string ConfigKeyVaultConfigEnvironmentVariable  = "MAMMON_CONFIG_KEYVAULT_URL";
     public const string MockCostAPIResponseFilePathConfigKey = "Mammon:MockCostAPIResponseFilePath";
     public const string CostCentreRuleEngineFilePathConfigKey = "Mammon:CostCentreRuleEngineFilePath";
 
