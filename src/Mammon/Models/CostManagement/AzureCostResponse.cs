0a03879cc9718e5cc70d70fb85415d0a11c1e58d
VaclavK <vaclav@vaclavkabat.net>
2024-06-04 11:57:28 +0100

    remove unused using

--- src/Mammon/Models/CostManagement/AzureCostResponse.cs
+++ src/Mammon/Models/CostManagement/AzureCostResponse.cs
@@ -1,40 +1,39 @@
-﻿using Newtonsoft.Json;
-using System.Diagnostics.CodeAnalysis;
+﻿using System.Diagnostics.CodeAnalysis;
 
 namespace Mammon.Models.CostManagement;
 
 public class AzureCostResponse : List<ResourceCostResponse>
 {
-    public string Currency => this.FirstOrDefault()?.Cost?.Currency ?? "N/A";
+	public string Currency => this.FirstOrDefault()?.Cost?.Currency ?? "N/A";
 }
 
 public record ResourceCostResponse
 {
-    public required string ResourceId { get; set; }
-    public required ResourceCost Cost { get; set; }
-    public required Dictionary<string, string> Tags { get; set; }
+	public required string ResourceId { get; set; }
+	public required ResourceCost Cost { get; set; }
+	public required Dictionary<string, string> Tags { get; set; }
 }
 
 public record ResourceCost
 {
 	[SetsRequiredMembers]
 	public ResourceCost(double costValue, string currency)
-    {
-        Cost = costValue;
+	{
+		Cost = costValue;
 		Currency = currency;
-    }
+	}
 
 	[SetsRequiredMembers]
-    public ResourceCost(IEnumerable<ResourceCost> costs)
-    {
-        if (!costs.All(x => x.Currency == costs.First().Currency))
-            throw new ArgumentException("All costs must have the same currency");
+	public ResourceCost(IEnumerable<ResourceCost> costs)
+	{
+		if (!costs.All(x => x.Currency == costs.First().Currency))
+			throw new ArgumentException("All costs must have the same currency");
 
-        Cost = costs.Sum(x => x.Cost);
-        Currency = costs.FirstOrDefault()?.Currency ?? "NA";
-    }
+		Cost = costs.Sum(x => x.Cost);
+		Currency = costs.FirstOrDefault()?.Currency ?? "NA";
+	}
 
-    public required double Cost { get; set; }
+	public required double Cost { get; set; }
 	public required string Currency { get; set; }
 
 	public override string ToString()
