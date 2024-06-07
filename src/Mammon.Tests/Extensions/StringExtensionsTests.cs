72fade85b2b77542fef446ddf8bfcb13b8bec14c
VaclavK <vaclav@vaclavkabat.net>
2024-06-07 15:44:11 +0100

    whitespace fix

--- src/Mammon.Tests/Extensions/StringExtensionsTests.cs
+++ src/Mammon.Tests/Extensions/StringExtensionsTests.cs
@@ -3,35 +3,35 @@
 [TestClass, TestCategory("UnitTest")]
 public class StringExtensionsTests
 {
-    [DataTestMethod]
-    [DataRow("/subscriptions/blah/resourceGroups/rgName/providers/Microsoft.ContainerService/managedClusters/resourceAKS/providers/blah", "/subscriptions/blah/resourceGroups/rgName/providers/Microsoft.ContainerService/managedClusters/resourceAKS")]
-    public void ToResourceActorIdTests(string input, string expected)
-    {
-        input.ToParentResourceId().Should().Be(expected);
-    }
+	[DataTestMethod]
+	[DataRow("/subscriptions/blah/resourceGroups/rgName/providers/Microsoft.ContainerService/managedClusters/resourceAKS/providers/blah", "/subscriptions/blah/resourceGroups/rgName/providers/Microsoft.ContainerService/managedClusters/resourceAKS")]
+	public void ToResourceActorIdTests(string input, string expected)
+	{
+		input.ToParentResourceId().Should().Be(expected);
+	}
 
 	[DataTestMethod]
-    [DataRow("CoreSuffix", "Core", "Suffix")]
+	[DataRow("CoreSuffix", "Core", "Suffix")]
 	[DataRow("CoreSuffix", "CoreSuffix", "blah")]
 	[DataRow("CoreSuffix-NE", "Core", "Suffix-NE", "Suffix")]
 	[DataRow("CoreSuffix", "Core", "Suffix-NE", "Suffix")]
 	[DataRow("CoreSuffix", "CoreSuffix", null)]
 	[DataRow(null, null, null)]
 	public void RemoveSuffixesTests(string input, string expected, params string[] suffixes)
-    {
-        var ret = input.RemoveSuffixes(suffixes);
+	{
+		var ret = input.RemoveSuffixes(suffixes);
 
-        ret.Should().Be(expected);
-    }
+		ret.Should().Be(expected);
+	}
 
-    [DataTestMethod]
-    [DataRow("a@b.c, d@e.f ","a@b.c", "d@e.f")]
+	[DataTestMethod]
+	[DataRow("a@b.c, d@e.f ", "a@b.c", "d@e.f")]
 	[DataRow("a@b.c, ", "a@b.c")]
-    [DataRow("")]
+	[DataRow("")]
 	[DataRow(null)]
 	public void SplitEmailContactsTests(string input, params string[] expectedItems)
-    {
-        var ret = input.SplitEmailContacts();
+	{
+		var ret = input.SplitEmailContacts();
 
 		ret.Should().BeEquivalentTo(expectedItems);
 	}
