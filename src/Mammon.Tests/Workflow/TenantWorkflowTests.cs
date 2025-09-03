7c2d9f7ad938183c50cf9fe6291f9dcbd9fac318
Adrian Andrei <aandrei@uniphar.ie>
2025-09-03 15:00:57 +0300

    more nits.

--- src/Mammon.Tests/Workflow/TenantWorkflowTests.cs
+++ src/Mammon.Tests/Workflow/TenantWorkflowTests.cs
@@ -3,227 +3,228 @@
 [TestClass, TestCategory("IntegrationTest")]
 public class TenantWorkflowTests
 {
-	private static ServiceBusClient? _serviceBusClient;
-	private static ServiceBusSender? _serviceBusSender;
-	private static BlobServiceClient? _blobServiceClient;
-	private static BlobContainerClient? _blobContainerClient;
-	private static ICslQueryProvider? _cslQueryProvider;
-	private static CostCentreRuleEngine? _costCentreRuleEngine;
-	private static IHost? _host;
-	private static IConfiguration? _config;
-	private static CostReportRequest _reportRequest = new()
-	{
-		ReportId = Guid.NewGuid().ToString(),
-		CostFrom = DateTime.UtcNow.BeginningOfDay().AddDays(-2),
-		CostTo = DateTime.UtcNow.BeginningOfDay().AddDays(-1)
-	};
+    private static ServiceBusClient? _serviceBusClient;
+    private static ServiceBusSender? _serviceBusSender;
+    private static BlobServiceClient? _blobServiceClient;
+    private static BlobContainerClient? _blobContainerClient;
+    private static ICslQueryProvider? _cslQueryProvider;
+    private static CostCentreRuleEngine? _costCentreRuleEngine;
+    private static IHost? _host;
+    private static IConfiguration? _config;
 
-	private static string? _reportSubject = string.Empty;
-	private static string? _fromEmail = string.Empty;
-	private static string? _toEmail = string.Empty;
-	private static CancellationToken _cancellationToken;
+    private static CostReportRequest _reportRequest = new()
+    {
+        ReportId = Guid.NewGuid().ToString(),
+        CostFrom = DateTime.UtcNow.BeginningOfDay().AddDays(-2),
+        CostTo = DateTime.UtcNow.BeginningOfDay().AddDays(-1)
+    };
+
+    private static string? _reportSubject = string.Empty;
+    private static string? _fromEmail = string.Empty;
+    private static string? _toEmail = string.Empty;
+    private static CancellationToken _cancellationToken;
 
 
-	[ClassInitialize]
-	public static void Initialize(TestContext testContext)
-	{
-		_cancellationToken = testContext.CancellationTokenSource.Token;
+    [ClassInitialize]
+    public static void Initialize(TestContext testContext)
+    {
+        _cancellationToken = testContext.CancellationTokenSource.Token;
 
-		var kvUrl = Environment.GetEnvironmentVariable(Consts.ConfigKeyVaultConfigEnvironmentVariable);
-		ArgumentException.ThrowIfNullOrWhiteSpace(kvUrl, nameof(kvUrl));
+        var kvUrl = Environment.GetEnvironmentVariable(Consts.ConfigKeyVaultConfigEnvironmentVariable);
+        ArgumentException.ThrowIfNullOrWhiteSpace(kvUrl, nameof(kvUrl));
 
-		DefaultAzureCredential azureCredential = new();
+        DefaultAzureCredential azureCredential = new();
 
-		/* the path matches two repos side by side checkout and running tests under standard bin/{config}/{netx.y}
-		 * this is the expected directory structure in local dev as well as the setup in CI/CD pipelines
-		 */
+        /* the path matches two repos side by side checkout and running tests under standard bin/{config}/{netx.y}
+         * this is the expected directory structure in local dev as well as the setup in CI/CD pipelines
+         */
 
-		var costCentreFile = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)!, "../../../../../../costcentre-definitions/costCentreRules.json");
+        var costCentreFile = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)!, "../../../../../../costcentre-definitions/costCentreRules.json");
 
-		var inMemorySettings = new List<KeyValuePair<string, string>> {
-			new(Consts.CostCentreRuleEngineFilePathConfigKey, costCentreFile)
-		};
+        var inMemorySettings = new List<KeyValuePair<string, string>>
+        {
+            new(Consts.CostCentreRuleEngineFilePathConfigKey, costCentreFile)
+        };
 
-		HostApplicationBuilder builder = new();
+        HostApplicationBuilder builder = new();
 
-		var policy = HttpPolicyExtensions
-		.HandleTransientHttpError() // HttpRequestException, 5XX and 408
-		.OrResult(response => (int)response.StatusCode == 429) // RetryAfter
-		.AddCostManagementRetryPolicy();
+        var policy = HttpPolicyExtensions
+            .HandleTransientHttpError() // HttpRequestException, 5XX and 408
+            .OrResult(response => (int)response.StatusCode == 429) // RetryAfter
+            .AddCostManagementRetryPolicy();
 
-		builder.Services
-			.AddTransient<AzureAuthHandler>()
-			.AddHttpClient("costRetrievalHttpClient")
-			.AddHttpMessageHandler<AzureAuthHandler>()
-			.AddPolicyHandler(policy);
+        builder.Services
+            .AddTransient<AzureAuthHandler>()
+            .AddHttpClient("costRetrievalHttpClient")
+            .AddHttpMessageHandler<AzureAuthHandler>()
+            .AddPolicyHandler(policy);
 
-		_host = builder.Build();
-		
-		ConfigurationBuilder configurationBuilder = new();
-		configurationBuilder.AddAzureKeyVault(new Uri(kvUrl), azureCredential);
-		configurationBuilder.AddInMemoryCollection(inMemorySettings!);
-		_config = configurationBuilder.Build();
+        _host = builder.Build();
 
-		var sbConnectionString = _config[Consts.DotFlyerSBConnectionStringConfigKey];
+        ConfigurationBuilder configurationBuilder = new();
+        configurationBuilder.AddAzureKeyVault(new Uri(kvUrl), azureCredential);
+        configurationBuilder.AddInMemoryCollection(inMemorySettings!);
+        _config = configurationBuilder.Build();
 
-		_reportSubject = _config[Consts.ReportSubjectConfigKey];
-		ArgumentException.ThrowIfNullOrWhiteSpace(_reportSubject);
+        var sbConnectionString = _config[Consts.DotFlyerSBConnectionStringConfigKey];
 
-		_fromEmail = _config[Consts.ReportFromAddressConfigKey];
-		ArgumentException.ThrowIfNullOrWhiteSpace(_fromEmail);
+        _reportSubject = _config[Consts.ReportSubjectConfigKey];
+        ArgumentException.ThrowIfNullOrWhiteSpace(_reportSubject);
 
-		_toEmail = _config[Consts.ReportToAddressesConfigKey];
-		ArgumentException.ThrowIfNullOrWhiteSpace(_toEmail);
+        _fromEmail = _config[Consts.ReportFromAddressConfigKey];
+        ArgumentException.ThrowIfNullOrWhiteSpace(_fromEmail);
 
-		_serviceBusClient = new(sbConnectionString, azureCredential);
-		_serviceBusSender = _serviceBusClient.CreateSender(Consts.MammonServiceBusTopicName);
+        _toEmail = _config[Consts.ReportToAddressesConfigKey];
+        ArgumentException.ThrowIfNullOrWhiteSpace(_toEmail);
 
-		var blobStorageConnectionString = _config[Consts.DotFlyerAttachmentsBlobStorageConnectionStringConfigKey]!;
-		_blobServiceClient = new(new Uri(blobStorageConnectionString), azureCredential);
+        _serviceBusClient = new(sbConnectionString, azureCredential);
+        _serviceBusSender = _serviceBusClient.CreateSender(Consts.MammonServiceBusTopicName);
 
-		var BlobStorageContainerName = _config[Consts.DotFlyerAttachmentsContainerNameConfigKey]!;
-		_blobContainerClient = _blobServiceClient.GetBlobContainerClient(BlobStorageContainerName);
+        var blobStorageConnectionString = _config[Consts.DotFlyerAttachmentsBlobStorageConnectionStringConfigKey]!;
+        _blobServiceClient = new(new Uri(blobStorageConnectionString), azureCredential);
 
-		_costCentreRuleEngine = new(_config);
-		
-		var _adxHostAddress = _config["AzureDataExplorer:HostAddress"];
+        var BlobStorageContainerName = _config[Consts.DotFlyerAttachmentsContainerNameConfigKey]!;
+        _blobContainerClient = _blobServiceClient.GetBlobContainerClient(BlobStorageContainerName);
 
-		var kcsb = new KustoConnectionStringBuilder(_adxHostAddress, "devops")		
-			.WithAadTokenProviderAuthentication(async () =>
-				(await azureCredential.GetTokenAsync(new(["https://kusto.kusto.windows.net/.default"]), cancellationToken: _cancellationToken)).Token);
+        _costCentreRuleEngine = new(_config);
 
-		_cslQueryProvider = KustoClientFactory.CreateCslQueryProvider(kcsb);
-	}
+        var _adxHostAddress = _config["AzureDataExplorer:HostAddress"];
 
-	[TestMethod]
-	public async Task WorkflowFinishesEmailSentTotalsMatch()
-	{
+        var kcsb = new KustoConnectionStringBuilder(_adxHostAddress, "devops")
+            .WithAadTokenProviderAuthentication(async () =>
+                (await azureCredential.GetTokenAsync(new(["https://kusto.kusto.windows.net/.default"]), cancellationToken: _cancellationToken)).Token);
 
-		//send report request to SB Topic to wake up Mammon instance
+        _cslQueryProvider = KustoClientFactory.CreateCslQueryProvider(kcsb);
+    }
 
-		///workaround for https://github.com/Azure/azure-cli/issues/28708#issuecomment-2047256166
-		///pre-access some things upfront
+    [TestMethod]
+    public async Task WorkflowFinishesEmailSentTotalsMatch()
+    {
+        //send report request to SB Topic to wake up Mammon instance
 
-		await _blobContainerClient!.ExistsAsync();
+        ///workaround for https://github.com/Azure/azure-cli/issues/28708#issuecomment-2047256166
+        ///pre-access some things upfront
 
-		var apiTotal = await ComputeCostAPITotalAsync();
+        await _blobContainerClient!.ExistsAsync();
 
-		await _serviceBusSender!.SendMessageAsync(new ServiceBusMessage
-		{
-			Body = BinaryData.FromObjectAsJson(new
-			{
-				data = _reportRequest
-			}),
-			ContentType = "application/json",
-		});
+        var apiTotal = await ComputeCostAPITotalAsync();
 
-		//wait for ADX to record email produced
-		string expectedSubject = string.Format(_reportSubject!, _reportRequest.ReportId);
-	
-		EmailData emailData = await _cslQueryProvider!
-			.WaitSingleQueryResult<EmailData>($"DotFlyerEmails | where Subject == \"{expectedSubject}\"", TimeSpan.FromMinutes(30), _cancellationToken);
+        await _serviceBusSender!.SendMessageAsync(new ServiceBusMessage
+        {
+            Body = BinaryData.FromObjectAsJson(new
+            {
+                data = _reportRequest
+            }),
+            ContentType = "application/json",
+        });
 
-		//assertions
-		var expectedToContacts = _toEmail!
-			.SplitEmailContacts()
-			.Select(e => new Contact { Name = e, Email = e });
+        //wait for ADX to record email produced
+        string expectedSubject = string.Format(_reportSubject!, _reportRequest.ReportId);
 
-		emailData.ToList.Should().BeEquivalentTo(expectedToContacts);
+        EmailData emailData = await _cslQueryProvider!
+            .WaitSingleQueryResult<EmailData>($"DotFlyerEmails | where Subject == \"{expectedSubject}\"", TimeSpan.FromMinutes(30), _cancellationToken);
 
-		emailData.Should().NotBeNull();
-		emailData.FromEmail.Should().Be(_fromEmail);
-		emailData.FromName.Should().Be(_fromEmail);
-		emailData.Attachments.Should().NotBeEmpty();
-		emailData.AttachmentsList.Should().ContainSingle();
-		emailData.ToList.Should().BeEquivalentTo(expectedToContacts);
+        //assertions
+        var expectedToContacts = _toEmail!
+            .SplitEmailContacts()
+            .Select(e => new Contact { Name = e, Email = e });
 
-		//retrieve content and compute total
-		var csvTotal = await ComputeCSVReportTotalAsync(emailData.AttachmentsList!.First().Uri);
+        emailData.ToList.Should().BeEquivalentTo(expectedToContacts);
 
-		// this is to cover rounding issues
-		var apiTotalRound = decimal.Round(apiTotal, 2);
-		var csvTotalRound = decimal.Round(csvTotal, 2);
+        emailData.Should().NotBeNull();
+        emailData.FromEmail.Should().Be(_fromEmail);
+        emailData.FromName.Should().Be(_fromEmail);
+        emailData.Attachments.Should().NotBeEmpty();
+        emailData.AttachmentsList.Should().ContainSingle();
+        emailData.ToList.Should().BeEquivalentTo(expectedToContacts);
 
-		(apiTotalRound - csvTotalRound).Should().BeLessThan(1m, $"api total is {apiTotalRound} and csv total is {csvTotalRound}");
-	}
+        //retrieve content and compute total
+        var csvTotal = await ComputeCSVReportTotalAsync(emailData.AttachmentsList!.First().Uri);
 
-	private static async Task<decimal> ComputeCostAPITotalAsync()
-	{
-		decimal total = 0m;
+        // this is to cover rounding issues
+        var apiTotalRound = decimal.Round(apiTotal, 2);
+        var csvTotalRound = decimal.Round(csvTotal, 2);
 
-		foreach (var subscription in _costCentreRuleEngine!.SubscriptionNames)
-		{
-			var request = new ObtainCostsActivityRequest
-			{
-				SubscriptionName = subscription,
-				CostFrom = _reportRequest.CostFrom,
-				CostTo = _reportRequest.CostTo,
-				PageIndex = 0,
-				GroupingMode = GroupingMode.Subscription
-			};
+        (apiTotalRound - csvTotalRound).Should().BeLessThan(1m, $"api total is {apiTotalRound} and csv total is {csvTotalRound}");
+    }
 
-			var httpClientFactory = _host!.Services.GetRequiredService<IHttpClientFactory>();
-			CostRetrievalService _costRetrievalService = new(new ArmClient(new DefaultAzureCredential()), httpClientFactory.CreateClient("costRetrievalHttpClient"), Mock.Of<ILogger<CostRetrievalService>>(), _config!);
+    private static async Task<decimal> ComputeCostAPITotalAsync()
+    {
+        decimal total = 0m;
 
-			var response = await _costRetrievalService!.QueryForSubAsync(request);
-			total += response.TotalCost;
-		}
+        foreach (var subscription in _costCentreRuleEngine!.SubscriptionNames)
+        {
+            var request = new ObtainCostsActivityRequest
+            {
+                SubscriptionName = subscription,
+                CostFrom = _reportRequest.CostFrom,
+                CostTo = _reportRequest.CostTo,
+                PageIndex = 0,
+                GroupingMode = GroupingMode.Subscription
+            };
 
-		return total;
-	}
+            var httpClientFactory = _host!.Services.GetRequiredService<IHttpClientFactory>();
+            CostRetrievalService _costRetrievalService = new(new ArmClient(new DefaultAzureCredential()), httpClientFactory.CreateClient("costRetrievalHttpClient"),
+                Mock.Of<ILogger<CostRetrievalService>>(), _config!);
 
-	private static async Task<decimal> ComputeCSVReportTotalAsync(string uri)
-	{
-		Uri blobUri = new(uri);
-		var blobName = blobUri.Segments.Last();
-		var fileName = Path.GetTempFileName();
-		var stream = await _blobContainerClient!.GetBlobClient(blobName).DownloadToAsync(fileName);
+            var response = await _costRetrievalService!.QueryForSubAsync(request);
+            total += response.TotalCost;
+        }
 
-		CsvConfiguration csvConfiguration = new(CultureInfo.InvariantCulture)
-		{
-			HasHeaderRecord = true,
-			HeaderValidated = null,
-			Delimiter = ",",
-			IgnoreBlankLines = true,
-			TrimOptions = TrimOptions.Trim,
-			MissingFieldFound = (args) => {}
-		};
+        return total;
+    }
 
-		CsvReader csvReader = new(new StreamReader(fileName), csvConfiguration);
-		csvReader.Read();
-		var headerRead = csvReader.ReadHeader();
-		csvReader.Read();
-		csvReader.GetRecord<object>(); //comment row to be read
+    private static async Task<decimal> ComputeCSVReportTotalAsync(string uri)
+    {
+        Uri blobUri = new(uri);
+        var blobName = blobUri.Segments.Last();
+        var fileName = Path.GetTempFileName();
+        var stream = await _blobContainerClient!.GetBlobClient(blobName).DownloadToAsync(fileName);
 
-		decimal total = 0m;
+        CsvConfiguration csvConfiguration = new(CultureInfo.InvariantCulture)
+        {
+            HasHeaderRecord = true,
+            HeaderValidated = null,
+            Delimiter = ",",
+            IgnoreBlankLines = true,
+            TrimOptions = TrimOptions.Trim,
+            MissingFieldFound = (args) => { }
+        };
 
-		while (csvReader.Read())
-		{
-			var lineCost = Convert.ToDecimal(csvReader.GetField(2), CultureInfo.InvariantCulture);
-			total += lineCost;
-		}
+        CsvReader csvReader = new(new StreamReader(fileName), csvConfiguration);
+        csvReader.Read();
+        var headerRead = csvReader.ReadHeader();
+        csvReader.Read();
+        csvReader.GetRecord<object>(); //comment row to be read
 
-		return total;
-	}
+        decimal total = 0m;
 
-	public class EmailData 
-	{
-		public required string FromEmail { get; set; }
+        while (csvReader.Read())
+        {
+            var lineCost = Convert.ToDecimal(csvReader.GetField(2), CultureInfo.InvariantCulture);
+            total += lineCost;
+        }
 
-		public required string FromName { get; set; }
+        return total;
+    }
 
-		public required string Attachments { get; set; }
+    public class EmailData
+    {
+        public required string FromEmail { get; set; }
 
-		public IList<Attachment>? AttachmentsList => JsonSerializer.Deserialize<IList<Attachment>>(Attachments);
+        public required string FromName { get; set; }
 
-		public IList<Contact>? ToList => JsonSerializer.Deserialize<IList<Contact>>(To);
+        public required string Attachments { get; set; }
 
-		public required string To { get; set; }
-	}
+        public IList<Attachment>? AttachmentsList => JsonSerializer.Deserialize<IList<Attachment>>(Attachments);
 
-	public class Attachment 
-	{
-		[JsonPropertyName("URI")]
-		public required string Uri { get; set; }
-	}
+        public IList<Contact>? ToList => JsonSerializer.Deserialize<IList<Contact>>(To);
+
+        public required string To { get; set; }
+    }
+
+    public class Attachment
+    {
+        [JsonPropertyName("URI")] public required string Uri { get; set; }
+    }
 }
