3fb3d250bc97cec6e192465fa590f28f37cc6704
Adrian Andrei <aandrei@uniphar.ie>
2025-09-19 10:10:00 +0300

    targetting day which was targetted previous testing days.

--- src/Mammon.Tests/Workflow/TenantWorkflowTests.cs
+++ src/Mammon.Tests/Workflow/TenantWorkflowTests.cs
@@ -14,8 +14,8 @@ public class TenantWorkflowTests
 	private static CostReportRequest _reportRequest = new()
 	{
 		ReportId = Guid.NewGuid().ToString(),
-        CostFrom = DateTime.UtcNow.BeginningOfDay().AddDays(-2),
-        CostTo = DateTime.UtcNow.BeginningOfDay().AddDays(-1)
+        CostFrom = DateTime.UtcNow.BeginningOfDay().AddDays(-17),
+        CostTo = DateTime.UtcNow.BeginningOfDay().AddDays(-16)
     };
 
 	private static string? _reportSubject = string.Empty;
