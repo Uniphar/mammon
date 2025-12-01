f7f7b1220ecf8ef34db7f9575aadb9bc5eafc3cc
Adrian Andrei <aandrei@uniphar.ie>
2025-12-01 14:41:17 +0200

    slight updates to AzureDevOpsAuthHandler

---
+++ src/Mammon/Utils/AzureDevOpsAuthHandler.cs
@@ -0,0 +1,14 @@
+﻿namespace Mammon.Utils;
+
+public class AzureDevOpsAuthHandler : DelegatingHandler
+{
+    protected const string AdoAppId = "499b84ac-1321-427f-aa17-267ca6975798";
+
+    protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+    {
+        var tokenResponse = await new DefaultAzureCredential().GetTokenAsync(new TokenRequestContext([$"{AdoAppId}/.default"]), cancellationToken);
+
+        request.Headers.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", tokenResponse.Token);
+        return await base.SendAsync(request, cancellationToken);
+    }
+}
