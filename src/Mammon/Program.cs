d02d14aa038d3f4660dcec5afcf7c75e61b527ce
Daniël Koek <contact@danielkoek.net>
2026-07-30 14:37:14 +0100

    Update Program.cs

--- src/Mammon/Program.cs
+++ src/Mammon/Program.cs
@@ -187,7 +187,7 @@ builder.Services
     .AddSingleton<CostCentreService>()
     .AddSingleton<LogAnalyticsService>()
     .AddSingleton<AKSService>()
-.AddSingleton<StateService>()
+    .AddSingleton<StateService>()
     .AddSingleton<SQLPoolService>()
     .AddSingleton<VDIService>()
     .AddSingleton<SqlFailoverService>()
