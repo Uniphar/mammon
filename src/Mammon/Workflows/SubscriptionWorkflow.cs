c2cb582caf85ef68ddf8a9a502a899a03a4a174f
Adrian Andrei <andrei.ionutadrian@gmail.com>
2026-08-14 18:02:14 +0300

    Refine SQL pool identification by using ResourceGroupName and Name for distinct selection

--- src/Mammon/Workflows/SubscriptionWorkflow.cs
+++ src/Mammon/Workflows/SubscriptionWorkflow.cs
@@ -75,7 +75,7 @@ public class SubscriptionWorkflow : Workflow<CostReportSubscriptionRequest, bool
 		}
 
 		//SQL Pool splitting
-		var sqlPools = costs.Where(x => x.IsSQLPool()).DistinctBy(t => t.ResourceId, StringComparer.OrdinalIgnoreCase);
+		var sqlPools = costs.Where(x => x.IsSQLPool()).DistinctBy(t => $"{t.ResourceIdentifier.ResourceGroupName}{t.ResourceIdentifier.Name}", StringComparer.OrdinalIgnoreCase);
 		foreach (var sqlPool in sqlPools)
 		{
 			await TriggerSplittableWorkflowAsync<SQLPoolWorkflow>(context, input, sqlPool);
