a30f115f5730b548951d2063c12b256cb006d6a5
Adrian Andrei <andrei.ionutadrian@gmail.com>
2026-08-14 18:42:23 +0300

    Refactor SQL pool identification to use ResourceIdentifier for improved clarity

--- src/Mammon/Workflows/SubscriptionWorkflow.cs
+++ src/Mammon/Workflows/SubscriptionWorkflow.cs
@@ -75,7 +75,7 @@ public class SubscriptionWorkflow : Workflow<CostReportSubscriptionRequest, bool
 		}
 
 		//SQL Pool splitting
-		var sqlPools = costs.Where(x => x.IsSQLPool()).DistinctBy(t => $"{t.ResourceIdentifier.ResourceGroupName}{t.ResourceIdentifier.Name}", StringComparer.OrdinalIgnoreCase);
+		var sqlPools = costs.Where(x => x.IsSQLPool()).DistinctBy(t => new ResourceIdentifier(t.ResourceId).Name, StringComparer.OrdinalIgnoreCase);
 		foreach (var sqlPool in sqlPools)
 		{
 			await TriggerSplittableWorkflowAsync<SQLPoolWorkflow>(context, input, sqlPool);
